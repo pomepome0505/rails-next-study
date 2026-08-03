e9b864f7b869a22900240567289a91ab9e7a72b4
pomepome0505 <ohaoha.0610@gmail.com>
2026-08-03 18:44:27 +0900

    Rails Local CIを導入

--- backend/config/ci.rb
+++ backend/config/ci.rb
@@ -7,17 +7,8 @@ CI.run do
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
-  step "Tests: Rails", "bin/rails test"
+
+  step "Tests: RSpec", "bin/rspec"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
-
-  # Optional: Run system tests
-  # step "Tests: System", "bin/rails test:system"
-
-  # Optional: set a green GitHub commit status to unblock PR merge.
-  # Requires the `gh` CLI and `gh extension install basecamp/gh-signoff`.
-  # if success?
-  #   step "Signoff: All systems go. Ready for merge and deploy.", "gh signoff"
-  # else
-  #   failure "Signoff: CI failed. Do not merge or deploy.", "Fix the issues and try again."
-  # end
+  step "Cleanup: Test DB", "env RAILS_ENV=test bin/rails db:truncate_all"
 end
